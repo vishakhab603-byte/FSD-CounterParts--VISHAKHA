495b0ababaa20a2f4dc3d9e4f6af2557185da97f
VISHAKHA <vishakhab603@gmail.com>
2026-07-29 14:35:23 +0530

    Add files via upload

---
+++ EXPERIMENT 2.1/COUNTERS/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: true
+    })
+})
